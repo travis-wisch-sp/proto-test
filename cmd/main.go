a67ddb3d5eea37dc1bc34efe16430bf2cd1c22a5
Travis-Wisch <travis.wisch@sailpoint.com>
2025-02-04 09:58:25 -0500

    Add a test identity and status for go example

--- cmd/main.go
+++ cmd/main.go
@@ -11,11 +11,17 @@ func main() {
 	fmt.Println("Testing generated proto files")
 
 	var testIdentities []*proto_go.Identity
+	testIdentities = append(testIdentities, &proto_go.Identity{
+		Id:          uuid.NewString(),
+		Type:        "IDENTITY",
+		SerialOrder: 1,
+	})
 
 	testApproval := proto_go.Approval{
 		Id:        uuid.NewString(),
 		TenantId:  uuid.NewString(),
 		Approvers: testIdentities,
+		Status:    proto_go.Status_PENDING,
 	}
 
 	fmt.Println("Test approval struct:")
